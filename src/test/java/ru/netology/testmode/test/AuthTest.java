3066b1cfcf477c53f15137074c6a5b662167328d
Evgenii <mikhailovich21@mail.ru>
2023-11-06 00:07:06 +0500

    Правка: удаление избыточных элементов в селекторах

--- src/test/java/ru/netology/testmode/test/AuthTest.java
+++ src/test/java/ru/netology/testmode/test/AuthTest.java
@@ -1,13 +1,14 @@
 package ru.netology.testmode.test;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.*;
 
 import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.*;
 import static ru.netology.testmode.data.DataGenerator.Registration.getRegisteredUser;
 import static ru.netology.testmode.data.DataGenerator.Registration.getUser;
@@ -24,51 +25,47 @@ class AuthTest {
     @Test
     void shouldSuccessfulLoginIfRegisteredActiveUser() {
         var registeredUser = getRegisteredUser("active");
-        $(".form [data-test-id='login'] .input__box .input__control").setValue(registeredUser.getLogin());
-        $(".form [data-test-id='password'] .input__box .input__control").setValue(registeredUser.getPassword());
-        $$("button").find(exactText("Продолжить")).click();
+        $(byXpath("//input[@name='login']")).setValue(registeredUser.getLogin());
+        $(byXpath("//input[@name='password']")).setValue(registeredUser.getPassword());
+        $(byXpath("//button[@data-test-id='action-login']")).click();
         $(byText("Личный кабинет")).shouldBe(visible);
     }
 
     @Test
     void shouldGetErrorIfNotRegisteredUser() {
         var notRegisteredUser = getUser("active");
-        $(".form [data-test-id='login'] .input__box .input__control").setValue(notRegisteredUser.getLogin());
-        $(".form [data-test-id='password'] .input__box .input__control").setValue(notRegisteredUser.getPassword());
-        $$("button").find(exactText("Продолжить")).click();
-        $(byText("Ошибка")).shouldBe(visible, Duration.ofSeconds(10));
-        $(withText("Неверно указан логин или пароль")).shouldBe(visible, Duration.ofSeconds(10));
+        $(byXpath("//input[@name='login']")).setValue(notRegisteredUser.getLogin());
+        $(byXpath("//input[@name='password']")).setValue(notRegisteredUser.getPassword());
+        $(byXpath("//button[@data-test-id='action-login']")).click();
+        $(byXpath("//div[@data-test-id='error-notification' and contains(., 'Неверно указан логин или пароль')]")).shouldBe(visible, Duration.ofSeconds(10));
     }
 
     @Test
     void shouldGetErrorIfBlockedUser() {
         var blockedUser = getRegisteredUser("blocked");
-        $(".form [data-test-id='login'] .input__box .input__control").setValue(blockedUser.getLogin());
-        $(".form [data-test-id='password'] .input__box .input__control").setValue(blockedUser.getPassword());
-        $$("button").find(exactText("Продолжить")).click();
-        $(byText("Ошибка")).shouldBe(visible, Duration.ofSeconds(10));
-        $(withText("Пользователь заблокирован")).shouldBe(visible, Duration.ofSeconds(10));
+        $(byXpath("//input[@name='login']")).setValue(blockedUser.getLogin());
+        $(byXpath("//input[@name='password']")).setValue(blockedUser.getPassword());
+        $(byXpath("//button[@data-test-id='action-login']")).click();
+        $(byXpath("//div[@data-test-id='error-notification' and contains(., 'Пользователь заблокирован')]")).shouldBe(visible, Duration.ofSeconds(10));
     }
 
     @Test
     void shouldGetErrorIfWrongLogin() {
         var registeredUser = getRegisteredUser("active");
         var wrongLogin = getRandomLogin();
-        $(".form [data-test-id='login'] .input__box .input__control").setValue(wrongLogin);
-        $(".form [data-test-id='password'] .input__box .input__control").setValue(registeredUser.getPassword());
-        $$("button").find(exactText("Продолжить")).click();
-        $(byText("Ошибка")).shouldBe(visible, Duration.ofSeconds(10));
-        $(withText("Неверно указан логин или пароль")).shouldBe(visible, Duration.ofSeconds(10));
+        $(byXpath("//input[@name='login']")).setValue(wrongLogin);
+        $(byXpath("//input[@name='password']")).setValue(registeredUser.getPassword());
+        $(byXpath("//button[@data-test-id='action-login']")).click();
+        $(byXpath("//div[@data-test-id='error-notification' and contains(., 'Неверно указан логин или пароль')]")).shouldBe(visible, Duration.ofSeconds(10));
     }
 
     @Test
     void shouldGetErrorIfWrongPassword() {
         var registeredUser = getRegisteredUser("active");
         var wrongPassword = getRandomPassword();
-        $(".form [data-test-id='login'] .input__box .input__control").setValue(registeredUser.getLogin());
-        $(".form [data-test-id='password'] .input__box .input__control").setValue(wrongPassword);
-        $$("button").find(exactText("Продолжить")).click();
-        $(byText("Ошибка")).shouldBe(visible, Duration.ofSeconds(10));
-        $(withText("Неверно указан логин или пароль")).shouldBe(visible, Duration.ofSeconds(10));
+        $(byXpath("//input[@name='login']")).setValue(registeredUser.getLogin());
+        $(byXpath("//input[@name='password']")).setValue(wrongPassword);
+        $(byXpath("//button[@data-test-id='action-login']")).click();
+        $(byXpath("//div[@data-test-id='error-notification' and contains(., 'Неверно указан логин или пароль')]")).shouldBe(visible, Duration.ofSeconds(10));
     }
 }
